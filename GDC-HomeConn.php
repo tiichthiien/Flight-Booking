bd10bf085f45b752ae2b34b17d961348d25c8534
atuaans <521h0516@student.tdtu.edu.vn>
2023-05-10 19:50:47 +0700

    check if form is submitted

--- GDC-HomeConn.php
+++ GDC-HomeConn.php
@@ -1,15 +1,22 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>GDC Airways - Flights</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Pacifico|Paytone+One" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Pacifico|Paytone+One" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="GDC-HomeConnCSSn.css">
 </head>
+
 <body>
 	<!--NavBar-->
 	<nav class="navbar navbar-inverse">
@@ -17,91 +24,86 @@
 			<div class="navbar-header">
 				<a href="index.php" class="navbar-brand"><img src="4b5d99c4-2f06-45be-88b2-5e4bb39f9885.png" id="logo"> GDC AIRWAYS</a>
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-	        		<span class="sr-only">Toggle navigation</span>
-	        		<span class="icon-bar"></span>
-	        		<span class="icon-bar"></span>
-	        		<span class="icon-bar"></span>
-	      </button>
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
 			</div>
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"">
-				<ul class="nav navbar-nav">
-					<li><a href="index.php"><strong>Home</strong></a></li>
-					<li><a href="GDC-About.php"><strong>About</strong></a></li>
-					<li><a href="GDC-Contacts.php"><strong>Contacts</strong></a></li>
-				</ul>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#" data-toggle="modal" data-target="#signup"><strong>Sign up </strong><i class="fas fa-user-plus"></i></a></li>
+					<li><a href="#" data-toggle="modal" data-target="#signup"><strong>Sign up</strong><i class="fas fa-user-plus"></i></a></li>
 					<li><a href="#" data-toggle="modal" data-target="#login"><strong>Login </strong><i class="fas fa-user"></i></a></li>
 				</ul>
 			</div>
 		</div>
 	</nav>
-   	<!--NavBar End-->
+	<!--NavBar End-->
 
-  	<!--SingUp Modal-->
-	  <div id="signup" class="modal fade" role="dialog">
-	    <div class="modal-dialog">
-	      <!--SignUp Modal content-->
-	      <div class="modal-content">
-	        <div class="modal-header">
-	          <button type="button" class="close" data-dismiss="modal">&times;</button>
-	          <h4 class="modal-title">SignUp</h4>
-	        </div>
-	        <div class="modal-body">
-	          	<label for="username">Username:</label>
-	            <input type="text" name="usernameSignUp" placeholder="Username" id="usernameSignUp">
-	            <br>
-	            <label for="email">Email ID:</label>
-	            <input type="email" name="emailid" placeholder="Email Id" id="email">
-	            <br>
-	            <label for="pass">Password:</label>
-	            <input type="password" name="passwordSignUp" id="passSignUp">
-	            <br>
-	            <label for="confpass">Confirm Password:</label>
-	            <input type="password" name="confpassword" id="confpass">
-	            <br>
-	            <input type="Submit" name="submit" id="submitSignUp">
-	            <br>
-	            <br>
-	            <span id="messagecheck1"></span>
-	            <br>
-	            <span id="messagecheck2"></span>
-	        </div>
-	        <div class="modal-footer">
-	          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-	        </div>
-	      </div>
-	      <!--SignUp Modal content End-->
-	    </div>
-	  </div>
-	  <!--SignUp Modal End-->
+	<!--SingUp Modal-->
+	<div id="signup" class="modal fade" role="dialog">
+		<div class="modal-dialog">
+			<!--SignUp Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">SignUp</h4>
+				</div>
+				<div class="modal-body">
+					<label for="username">Username:</label>
+					<input type="text" name="usernameSignUp" placeholder="Username" id="usernameSignUp">
+					<br>
+					<label for="email">Email ID:</label>
+					<input type="email" name="emailid" placeholder="Email Id" id="email">
+					<br>
+					<label for="pass">Password:</label>
+					<input type="password" name="passwordSignUp" id="passSignUp">
+					<br>
+					<label for="confpass">Confirm Password:</label>
+					<input type="password" name="confpassword" id="confpass">
+					<br>
+					<input type="Submit" name="submit" id="submitSignUp">
+					<br>
+					<br>
+					<span id="messagecheck1"></span>
+					<br>
+					<span id="messagecheck2"></span>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+			<!--SignUp Modal content End-->
+		</div>
+	</div>
+	<!--SignUp Modal End-->
 
-	  <!--Login Modal-->
-	  <div id="login" class="modal fade" role="dialog">
-	    <div class="modal-dialog">
-	      <!--Login Modal content-->
-	      <div class="modal-content">
-	        <div class="modal-header">
-	          <button type="button" class="close" data-dismiss="modal">&times;</button>
-	          <h4 class="modal-title">Login</h4>
-	        </div>
-	        <div class="modal-body">
-	          <label for="username">Username:</label>
-	          <input type="text" name="usernameLogin" placeholder="Username" id="usernameLogin">
-	          <br>
-	          <label for="pass">Password:</label>
-	          <input type="password" name="passwordLogin" id="passLogin">
-	          <br>
-	          <input type="Submit" name="submit">
-	        </div>
-	        <div class="modal-footer">
-	          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-	        </div>
-	      </div>
-	      <!--Login Modal Content End-->
-	    </div>
-	  </div>
-	  <!--Login Modal End-->
+	<!--Login Modal-->
+	<div id="login" class="modal fade" role="dialog">
+		<div class="modal-dialog">
+			<!--Login Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">Login</h4>
+				</div>
+				<div class="modal-body">
+					<label for="username">Username:</label>
+					<input type="text" name="usernameLogin" placeholder="Username" id="usernameLogin">
+					<br>
+					<label for="pass">Password:</label>
+					<input type="password" name="passwordLogin" id="passLogin">
+					<br>
+					<input type="Submit" name="submit">
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+			<!--Login Modal Content End-->
+		</div>
+	</div>
+	<!--Login Modal End-->
 
 	<div class="container">
 		<h1 class="hsty" id="he1">Departure</h1>
@@ -119,54 +121,52 @@
 			</thead>
 			<tbody>
 				<?php
-					$dbhost="127.0.0.1";
-					$dbuser="root";
-					$dbpass="";
-					$db="Flight_Reservation";
-					$conn=mysqli_connect($dbhost,$dbuser,$dbpass,$db);
-					if(!$conn)
-					{
-						echo "Connection was failed".mysqli_connect_error();
-					}
-					$origin=$_POST['origin'];
-					$origin=mysqli_real_escape_string($conn,$origin);
-					$destination=$_POST['destination'];
-					$destination=mysqli_real_escape_string($conn,$destination);
-					$depart=$_POST['depart'];
-					$return=$_POST['return'];
-					$passengers=$_POST['passengers'];
-					if($_POST['submit'])
-					{
-						$sql="Select * from flight where Origin='".$origin."' and Destination='".$destination."'";
-						if(mysqli_query($conn,$sql))
-						{
-							$print=mysqli_query($conn,$sql);
+				$dbhost = "127.0.0.1";
+				$dbuser = "root";
+				$dbpass = "";
+				$db = "Flight_Reservation";
+				$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
+				if (!$conn) {
+					echo "Connection was failed" . mysqli_connect_error();
+				}
+				if (isset($_POST['submit'])) {
+					if (isset($_POST['origin']) && isset($_POST['destination']) && isset($_POST['depart']) && isset($_POST['return']) && isset($_POST['passengers'])) {
+						$origin = mysqli_real_escape_string($conn, $_POST['origin']);
+						$destination = mysqli_real_escape_string($conn, $_POST['destination']);
+						$depart = $_POST['depart'];
+						$return = $_POST['return'];
+						$passengers = $_POST['passengers'];
 
-							while($row = mysqli_fetch_assoc($print))
-							{
+						$sql = "Select * from flight where Origin='" . $origin . "' and Destination='" . $destination . "'";
+						if (mysqli_query($conn, $sql)) {
+							$print = mysqli_query($conn, $sql);
+
+							while ($row = mysqli_fetch_assoc($print)) {
 								echo "<tr>";
-								echo "<td>".$row['FlightNo']."</td>";
+								echo "<td>" . $row['FlightNo'] . "</td>";
 								$var_value = $row['FlightNo'];
-								$var_value=mysqli_real_escape_string($conn,$var_value);
-								echo "<td>".$row['Type']."</td>";
-								echo "<td>".$row['Origin']."</td>";
-								echo "<td>".$row['Destination']."</td>";
-								echo "<td>".$row['DepartureTime']."</td>";
-								echo "<td>".$row['ArrivalTime']."</td>";
+								$var_value = mysqli_real_escape_string($conn, $var_value);
+								echo "<td>" . $row['Type'] . "</td>";
+								echo "<td>" . $row['Origin'] . "</td>";
+								echo "<td>" . $row['Destination'] . "</td>";
+								echo "<td>" . $row['DepartureTime'] . "</td>";
+								echo "<td>" . $row['ArrivalTime'] . "</td>";
 								echo "<td><form method='GET' action='GDC-Book.php'>
-    									  <input type='hidden' name='varname' value='$var_value'>
-    									  <input type='submit' value='BOOK' style='color:black'>
-										  </form></td>";
+														  <input type='hidden' name='varname' value='$var_value'>
+														  <input type='submit' value='BOOK' style='color:black'>
+														  </form></td>";
 								/*echo "<td><a href='GDC-Book.php?varname=<?php echo $var_value ?>'>Book</a></td>";*/
-								
+
 								echo "</tr>";
 							}
-						}
-						else
-						{
+						} else {
 							echo "Something Went Wrong";
 						}
+					} else {
+						echo "All fields are required";
 					}
+				}
+
 				?>
 			</tbody>
 		</table>
@@ -186,39 +186,34 @@
 			</thead>
 			<tbody>
 				<?php
-					$sqlarr="Select * from flight where Origin='".$destination."' and Destination='".$origin."'";
-					if(mysqli_query($conn,$sqlarr))
-						{
-							$print=mysqli_query($conn,$sqlarr);
+				$sqlarr = "Select * from flight where Origin='" . $destination . "' and Destination='" . $origin . "'";
+				if (mysqli_query($conn, $sqlarr)) {
+					$print = mysqli_query($conn, $sqlarr);
 
-							while($row = mysqli_fetch_assoc($print))
-							{
-								echo "<tr>";
-								echo "<td>".$row['FlightNo']."</td>";
-								$var_value = $row['FlightNo'];
-								$var_value=mysqli_real_escape_string($conn,$var_value);
-								echo "<td>".$row['Type']."</td>";
-								echo "<td>".$row['Origin']."</td>";
-								echo "<td>".$row['Destination']."</td>";
-								echo "<td>".$row['DepartureTime']."</td>";
-								echo "<td>".$row['ArrivalTime']."</td>";
-								echo "<td><form method='GET' action='GDC-Book.php'>
+					while ($row = mysqli_fetch_assoc($print)) {
+						echo "<tr>";
+						echo "<td>" . $row['FlightNo'] . "</td>";
+						$var_value = $row['FlightNo'];
+						$var_value = mysqli_real_escape_string($conn, $var_value);
+						echo "<td>" . $row['Type'] . "</td>";
+						echo "<td>" . $row['Origin'] . "</td>";
+						echo "<td>" . $row['Destination'] . "</td>";
+						echo "<td>" . $row['DepartureTime'] . "</td>";
+						echo "<td>" . $row['ArrivalTime'] . "</td>";
+						echo "<td><form method='GET' action='GDC-Book.php'>
     									  <input type='hidden' name='varname' value='$var_value'>
     									  <input type='submit' value='BOOK' style='color:black'>
 										  </form></td>";
-								echo "</tr>";
-							}
-						}
-						else
-						{
-							echo "Something Went Wrong";
-						}
+						echo "</tr>";
+					}
+				} else {
+					echo "Something Went Wrong";
+				}
 				?>
 			</tbody>
 		</table>
 	</div>
 
 </body>
+
 </html>
-
-
